39926a5260659b385c4fa66ac7789fc50c5cc466
JoanAndoni <mcr_joan@hotmail.com>
2020-08-05 09:26:56 -0500

    All

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ function cardScroll(idCard, direction) {
     const movement = (Math.floor(cards.offsetWidth / 250) * 250);
     direction === 'left' ? cards.scrollLeft -= movement : cards.scrollLeft += movement;
 }
+
+function changeLanguage(selectLanguage) {
+    if (selectLanguage.value === 'Español') {
+        // window.location.href = 'www.upmint.co';
+        window.location.replace('www.upmint.co');
+    }
+    if (selectLanguage.value === 'English') {
+        // window.location.href = 'en.upmint.co';
+        window.location.replace('en.upmint.co');
+    }
+}
